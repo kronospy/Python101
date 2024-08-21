462469130ac6f2a9c8148c6b0bc527a786b95bcf
yosra15 <yousraammar0@gmail.com>
2024-08-21 16:49:16 +0000

    exersice of lec1

---
+++ lectures/licture_1/grade.py
@@ -0,0 +1,13 @@
+score = int (input("score"))
+if score >= 90:
+    print("Grad: A")
+elif score >=80:
+    print("Grad B")
+elif score >=70:
+    print("Grad C")
+elif score >=60:
+    print("Grad D")
+else:
+    print("Grad F")
+
+    
