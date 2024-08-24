036b920fe322008b50da7ea0cadd2f2e878201eb
yosra15 <yousraammar0@gmail.com>
2024-08-24 14:30:26 +0000

    homework indoor 1

---
+++ indoore/indoor.py
@@ -0,0 +1,12 @@
+def main ():
+  HELLO =int("input HELLO")
+  print("hello")
+
+
+def indoor():
+    text = input("write Cap word: ").lower()
+    if text == '40':
+       text = '50'
+    print(text)
+
+indoor()
